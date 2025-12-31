651adea3f81209fc28efcc8336dec135a7ee2ee7
Aaditya <desh.aaditya165@gmail.com>
2025-12-31 13:27:31 +0530

    Add time tracking data for CatBoost iterations in time_left.tsv

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 from src.exception import CustomException
 from src.logger import logging
 import dill
+from sklearn.metrics import r2_score
 def save_object(file_path,obj):
     try:
         dir_path=os.path.dirname(file_path)
@@ -12,5 +13,21 @@ def save_object(file_path,obj):
 
         with open(file_path,'wb') as file_obj:
             dill.dump(obj,file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
+
+def evaluate_models(X,y,X_test,y_test,models):
+    try:
+        report={}
+        for i in range(len(models)):
+            model=list(models.values())[i]
+            model.fit(X,y)
+
+            y_test_pred=model.predict(X_test)
+
+            test_model_score=r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]]=test_model_score
+        return report
     except Exception as e:
         raise CustomException(e,sys)
